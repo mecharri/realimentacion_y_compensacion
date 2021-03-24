b3760661a19f04b1f4e70462ca0f87053a0bd27d
MartinE <echarri.martin@gmail.com>
2021-03-23 22:20:05 -0300

    Se tiene funcionando el bode de la ganancia de lazo y los polos de la realimentada, la respeusta al escalón con problemas. Se corrige README

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-= Datos de proyecto
-* Pycharm 
-* Python 3
+# Datos de proyecto
+* Proyecto Pycharm 
+* Python 3.8
 
-= Idea
+# Idea
 La idea de este proyecto es tener una herramienta para conceptualizar las diferencias entre ganancia de lazo, ganancia a lazo cerrado. La posición de las singularidades del sistema realimentado y su relación con la ganancia de lazo.
 
-= Contexto
+# Contexto
 Este proyecto está pensado para usar en el contexto de la materia Electrónica Analógica II de la carrera Ingeniería Electrónica de la UNSAM.
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import control as ctl
-import symbolic
+# import symbolic
 
 s = ctl.TransferFunction.s
-P1 = 50
-P2 = 100
-P3 = 10
-G = 100000/((s+P1)*(s+P2)*(s+P3))
+P1 = 1e3
+P2 = 100e3
+P3 = 500e3
+A0 = 1e3
+G = A0/((s/P1+1)*(s/P2+1)*(s/P3+1))
 k = 0.1
 H = G/(1+k*G)
 print(H)
@@ -20,5 +21,15 @@ y_zeros = ctl.zero(H).imag
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.scatter(x_polos,y_polos,marker="x")
-ax.scatter(x_zeros, y_zeros, marker="o")
+# ax.scatter(x_zeros, y_zeros, marker="o")
+
+tvec = np.linspace(0,1000,1000)
+tvec, yout= ctl.step_response(H, tvec)
+
+plt.figure()
+ctl.bode(H, dB=1, deg =1, margins=1)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(1,1,1)
+ax2.plot(tvec,yout)
 plt.show()
